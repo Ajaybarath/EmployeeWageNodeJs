0379d45740b51e56f3037b27626da1042c52a2b4
ajaybarath <ajaybarath3@gmail.com>
2021-10-09 13:22:30 +0530

    Added: name check with regex pattern

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -18,20 +18,34 @@ class EmployeePayrllData {
     }
 
     set name(name) {
-        this._name = name;
+        let namrRegex = RegExp('^[A-Z]{1}[a-z]{3,}$')
+        if (namrRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorect'
     }
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = this.startDate === undefined ? "undefined" : 
-        this.startDate.toLocaleDateString("en-US", options);
+        const empDate = this.startDate === undefined ? "undefined" :
+            this.startDate.toLocaleDateString("en-US", options);
         return "id= " + this.id + ", name= " + this.name + ", salary= " + this.salary + ", gender= " + this.gender + ", startDate= " + empDate;
     }
 }
 
 let empployeePayrollData = new EmployeePayrllData(1, "Mark", 30000);
 console.log(empployeePayrollData.toString());
-empployeePayrollData.name = "john";
-console.log(empployeePayrollData.toString());
-let newEmployeePayrollData = new EmployeePayrllData(2, "Tressia", 30000, "F", new Date());
-console.log(newEmployeePayrollData.toString());
+try {
+    empployeePayrollData.name = "john";
+    console.log(empployeePayrollData.toString());
+}
+catch (e) {
+    console.error(e)
+}
+try {
+
+    let newEmployeePayrollData = new EmployeePayrllData(2, "Tressia", 30000, "F", new Date());
+    console.log(newEmployeePayrollData.toString());
+}
+catch (e) {
+    console.error(e)
+}
